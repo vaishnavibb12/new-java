9c0361a462887967ea66dc2af2656b256916f1e3
vaishnavibb12 <vaishabiramane@gmail.com>
2025-04-19 13:41:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+// let score = 45;
+// let grade;
+
+// if (score > 40) {
+//     grade = 'A';
+// } else {
+//     grade = 'Not A';
+// }
+
+// console.log("Score:", score);
+// console.log("Grade:", grade);
+
+
+// for(var i=0;i<20;i++){
+// if (i%3==0) {
+//     console.log(i,'Fpo');
+    
+// } else {
+//     console.log(i,'DfuLT');
+    
+    
+// }
+// }
+
+for (let i = 1; i <= 5; i++) {
+    switch(i) {
+        case 1:
+            console.log("Number is One");
+            break;
+        case 2:
+            console.log("Number is Two");
+            break;
+        case 3:
+            console.log("Number is Three");
+            break;
+        case 4:
+            console.log("Number is Four");
+            break;
+        case 5:
+            console.log("Number is Five");
+            break;
+        default:
+            console.log("Number is out of range");
+    }
+}
+
+
+
+
